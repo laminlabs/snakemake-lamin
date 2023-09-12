fd46094ae11b7b8af8aed3edc51442b5f8aa5629
zethson <lukas.heumos@posteo.net>
2023-09-12 17:17:16 +0200

    :sparkles: Execute notebook

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -9,8 +9,5 @@ def test_notebooks():
     docs_folder = Path(__file__).parents[1] / "docs/"
 
     for check_folder in docs_folder.glob("./**"):
-        # these are the notebook testpaths
-        if not str(check_folder).endswith(("guide", "faq")):
-            continue
         logger.debug(f"\n{check_folder}")
         test.execute_notebooks(check_folder, write=True)
