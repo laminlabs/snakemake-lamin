23fc237cfd1665b8496fb43babae93f6f6849e27
Alex Wolf <f.alexander.wolf@gmail.com>
2025-12-14 23:42:41 +0100

    Fix

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 from laminci import upload_docs_artifact
-from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+from laminci.nox import build_docs, run_pre_commit, run_pytest
 
 # we'd like to aggregate coverage information across sessions
 # and for this the code needs to be located in the same
