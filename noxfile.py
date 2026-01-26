4514478b08dcf103689a7cbb5f8c5ef0138a27d1
Alex Wolf <f.alexander.wolf@gmail.com>
2026-01-26 16:59:57 +0100

    👷 Polish repo setup (#9)
    * Polish
    
    * Fix
    
    * No lamindb from pypi
    
    * Fix
    
    * Fix

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,9 @@
 import nox
+import os
 from laminci import upload_docs_artifact
-from laminci.nox import build_docs, run_pre_commit, run_pytest
+from laminci.nox import build_docs, run_pre_commit, run, install_lamindb
+
+IS_PR = os.getenv("GITHUB_EVENT_NAME") != "push"
 
 # we'd like to aggregate coverage information across sessions
 # and for this the code needs to be located in the same
@@ -16,7 +19,9 @@ def lint(session: nox.Session) -> None:
 
 @nox.session()
 def build(session):
-    session.run(*"pip install -e .[dev]".split())
-    run_pytest(session)
-    build_docs(session, strict=True)
+    branch = "main" if IS_PR else "release"
+    install_lamindb(session, branch=branch)
+    run(session, "uv pip install --system pytest")
+    run(session, "pytest -s ./tests/test_notebooks.py")
+    build_docs(session, strict=False)
     upload_docs_artifact(aws=True)
